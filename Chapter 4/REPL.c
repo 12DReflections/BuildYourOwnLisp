762a4ef70b70f5a5d6aa1b3189f5fca515068768
Julian Wise - D <Julianadamwise@gmail.com>
2015-11-15 13:47:44 +1100

    Defined Windows and Linux Loading

--- Chapter 4/REPL.c
+++ Chapter 4/REPL.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int arc, char ** argv) {
+/* If compiling on windows */
+#ifdef _WIN32
+#include <string.h> 
+char * readline( char *prompt );
+/* Static global variable is only seen within the file it's declared in  
+	Static local variable can is saved between invocations
+	size(input) = 2048 = 2^10 
+*/
+static char buffer[2048];
 
-	/* Static global variable is only seen within the file it's declared in  
-		Static local variable can is saved between invocations
-		size(input) = 2048 = 2^10 
-	*/
-	static char input[2048];
+/* Fake readline function */
+char * readline( char *prompt ) {
+	fputs( prompt, stdout ); 
+	fgets( buffer, sizeof(buffer), stdin );
+	char *cpy = malloc( strlen(buffer)+1 );
+	strcpy( cpy, buffer );
+	cpy[strlen( cpy )-1] = '\0';
+	return cpy;
+}
+
+/* Fake add history function */
+void add_history( char* unused ){};
+
+/* Otherwise include editline headers */
+#else
+/* to use the editline library 
+	Linux: sudo apt-get install libedit -dev */
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+
+int main(int argc, char ** argv) {
+
 	
 	printf("JLISP Version 0.0.0.0.1\n");
-	printf("\tEnter Ctrl+c to exit\n\t(or Ctrl+d on LINUX)\n\n");
+	printf("\tEnter Ctrl+c to exit\n\n");
 
-	while( *input != EOF){
+	while( 1 ){
 
-		/* Print and don't append newline */
-		fputs("JLisp> ", stdout);
-
-		fgets(input, sizeof(input), stdin);
-		printf("No you're a %s", input);
-
+		char *input = readline( "JLISP> ");
+		add_history( input );
+		printf( "No you're a %s\n", input );
+		free( input );
 	}
 
 	return 0;
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BuildYourOwnLisp
 Building a LISP Compiler in C
+
+This my attempt and building a LISP Compiler in C named JLISP.
+The core structure of JLISP will be derived from Daniel Holden's book 
+"Build your own LISP"
+http://www.buildyourownlisp.com/
+
+BYO Lisp runs on windows out of the box.
+To run in Linux install libedit
+sudo apt-get install libedit -dev
